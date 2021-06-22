99baed2e1664220c971bf8e395a3020183e0a730
Andrew Blignaut <andrew.blignaut@statpro.com>
2021-06-22 15:54:37 +0200

    add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,21 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import bootstrap from "bootstrap/dist/js/bootstrap"
+import "../stylesheets/application.scss"
+
+document.addEventListener('turbolinks:load', () => {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+})
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'main#index'
   resources :games
   resources :unknown_risks
   resources :complexities
